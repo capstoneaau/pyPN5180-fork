826b128e538de289b925468055815f377b71afbf
kquote03 <kquote03@gmail.com>
2025-05-30 08:31:57 +0400

    removed old actiavte type a method

--- PN5180/ISO14443.py
+++ PN5180/ISO14443.py
@@ -9,15 +9,11 @@ class ISO14443(AbstractPN5180):
 		self.rf_on()
 		print(self.get_irq_status())
 	
-	def activate_type_a(self, kind: ISO14443InitCommand) -> int:
-		'''
-		Determines length of UID (currently only 4 and 7 byte UIDs are supported)
-		TODO:
-			- Error checking
-			- Refactor clear and enable CRC's RX and TX methods 
-		'''
-		uid_length = 0
-
+	def activate_type_a(self, kind: ISO14443InitCommand) -> tuple:
+		"""
+		Internal method to activate Type A card and return full response
+		Returns tuple: (atqa, sak, uid_bytes) or None if failed
+		"""
 		self.disable_crypto()
 		self.disable_crc()
 		self.clear_IRQ_STATUS()  # Clears the interrupt register IRQ_STATUS
@@ -42,27 +38,9 @@ class ISO14443(AbstractPN5180):
 		self.send_data([0x93, 0x70]+buff2,0x00)
 
 		buff = buff + self.read_data(1)
-
-		if (buff[2] & 0x04) == 0:
-			buff = buff + buff2
-			uid_length = 4
-		else:
-			if buff2[0] != 0x88:
-				raise Exception("err comparing uid buffer")
-			buff = buff + buff2[1:]
-			self.write_register_with_and_mask(CRC_RX_CONFIG, 0xFFFFFFFE)
-			self.write_register_with_and_mask(CRC_TX_CONFIG, 0xFFFFFFFE)
-			self.send_data([0x95, 0x20], 0x00)
-			buff3 = self.read_data(5)
-			buff3 = buff3 + buff
-			self.write_register_with_or_mask(CRC_RX_CONFIG, 0x01)
-			self.write_register_with_or_mask(CRC_TX_CONFIG, 0x01)
-			self.send_data([0x95, 0x70]+buff3,0x00)
-			sak = self.read_data(1)
-			uid_length = 7
-
-		print(uid_length)
-		return(uid_length)
+print(buff)
+		print(buff2)
+		return (buff[0], buff[2], buff2[:-1])
 
 	def rx_bytes_received(self) -> int:
 		"""
